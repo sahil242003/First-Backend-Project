237337bb105d660fb528501ab276ad03d47f0929
sahil ahire <sahilahire2003@gmail.com>
2026-02-08 16:31:58 +0530

    add utils code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { configDotenv } from "dotenv";
 
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 
 configDotenv()
 
@@ -9,5 +10,14 @@ configDotenv({
 })
 
 connectDb()
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log(`Mongo DB connection failed !!!`, err);
+    
+})
 
