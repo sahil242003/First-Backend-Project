4f768bee82b26da13586fe3d73df1881405e8e97
sahil ahire <sahilahire2003@gmail.com>
2026-03-01 11:52:29 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -13,3 +13,29 @@ connectDB()
 .catch((error) => {
     console.log(`MONGODB connection FAILED !!`, error);
 })
+
+
+//other way without creating seperate index.db.js file
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
